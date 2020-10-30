3f5956fa2543a7ea4acd0268e0a4cc2094038caa
Byron Brown <Beezy@MacBook-4.local>
2020-10-29 22:51:04 -0500

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const app = express();
 require('./database');
 
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -28,5 +29,8 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5020;
 app.listen(port, () => {
+
+    
+
     console.log(`Server started on port ${port}`);
 });
